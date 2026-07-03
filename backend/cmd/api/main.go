9c988f6ffcec9e6813f0a4d49855d4f66e72dd1b
nhan <trannhanlv2004@gmail.com>
2026-07-04 01:02:09 +0700

    add(backend): add main file

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/trannhanlv2004/team-tickets/internal/app"
+	"github.com/trannhanlv2004/team-tickets/internal/configs"
+	"github.com/trannhanlv2004/team-tickets/internal/database"
+	"github.com/trannhanlv2004/team-tickets/internal/logger"
+	"github.com/trannhanlv2004/team-tickets/internal/routes"
+)
+
+func main() {
+	appConfig, err := configs.LoadAppConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	appLogger, cleanup, err := logger.New(appConfig.LogDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cleanup()
+
+	appLogger.App.Printf(
+		"App running in %s mode on port %s",
+		appConfig.Env,
+		appConfig.Port,
+	)
+
+	r := gin.Default()
+
+	db, err := database.ConnectPostgres(*appConfig.DB)
+	if err != nil {
+		appLogger.Error.Printf("failed to connect postgres: %v", err)
+		panic(err)
+	}
+
+	dependencies := app.NewDependencies(db)
+	routes.RegisterV1(r, dependencies)
+
+	if err := r.Run(":" + appConfig.Port); err != nil {
+		appLogger.Error.Printf("failed to run server: %v", err)
+		panic(err)
+	}
+}
